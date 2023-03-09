b099fade177cb90df07b132c895c0b28925f236b
Denis Junior <denis_jr2001@hotmail.com>
2023-03-09 12:13:18 -0300

    context running

--- src/context/Context.jsx
+++ src/context/Context.jsx
@@ -4,33 +4,32 @@ const Context = createContext({});
 
 export function ContextProvider({ children }) {
   const [passwords, setPasswords] = useState([]);
-  
   const [currentPassword, setCurrentPassword] = useState(0);
 
   const handlePassword = (newPassword) => {
-      setPasswords((oldData)=>[newPassword, ...oldData])
+    setPasswords((oldData) => [newPassword, ...oldData]);
   };
 
-  const trololo = (e) => {
+  const getLocalStoragePasswords = (e) => {
     const { key, newValue } = e;
     if (key === "pass") {
-        console.log(newValue)
       setPasswords(JSON.parse(newValue));
     }
   };
-  
+
   useEffect(() => {
-    const meuStorage = localStorage.getItem("pass")
-    if (meuStorage?.length) setPasswords(JSON.parse(meuStorage));
-    window.addEventListener("storage", trololo);
+    const myStorage = localStorage.getItem("pass");
+    if (myStorage?.length) setPasswords(JSON.parse(myStorage));
+    window.addEventListener("storage", getLocalStoragePasswords);
     return () => {
-      window.removeEventListener("storage", trololo);
+      window.removeEventListener("storage", getLocalStoragePasswords);
     };
   }, []);
 
-    useEffect(() => {
-        if (passwords.length) localStorage.setItem("pass", JSON.stringify(passwords));
-    }, [passwords]);
+  useEffect(() => {
+    if (passwords.length)
+      localStorage.setItem("pass", JSON.stringify(passwords));
+  }, [passwords]);
 
   return (
     <Context.Provider value={{ passwords, handlePassword }}>
